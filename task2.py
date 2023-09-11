151bbf870d114755cc04175e0c0a2030a8967380
pavithra1252004 <144316318+pavithra1252004@users.noreply.github.com>
2023-09-11 11:35:15 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,9 @@
+a=int(input("Enter The Value1:"))
+b=int(input("Enter The Value2:"))
+c1=a+b
+c2=a-b
+c3=a*b
+print("Sum of Two Numbers is:",c1)
+print("Difference of Two Numbers is:",c2)
+print("Product of Two Numbers is:",c3)
+
